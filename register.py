2cc37ddc5857352d19631ba2191daf6582a7e1df
MaxaM <94958239+Nagatochyan@users.noreply.github.com>
2023-01-23 11:36:20 +0900

    Add files via upload

---
+++ register.py
@@ -0,0 +1,25 @@
+import sqlite3
+import subprocess
+dbname = 'main.db'
+conn = sqlite3.connect(dbname)
+
+# SQLiteを操作するためのカーソルを作成
+cur = conn.cursor()
+
+# テーブルの作成
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS hwidauth (hwid text)"
+)
+def GetUUID():
+    cmd = 'wmic csproduct get uuid'
+    uuid = str(subprocess.check_output(cmd))
+    pos1 = uuid.find("\\n")+2
+    uuid = uuid[pos1:-15]
+    cur.execute("INSERT INTO hwidauth VALUES (?)", [uuid])
+    conn.commit()
+
+GetUUID()
+cur.execute('SELECT * FROM hwidauth')
+for row in cur:
+    print(row)
+conn.close()
